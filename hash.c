dd72ac81c06aa0b932872cd0f87cc10ccc0394f4
kyo <gongruiyin@RainMac.local>
2017-05-14 22:46:07 +0900

    new

---
+++ hash.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void _zend_hash_init(HashTable *ht, int nSize, dtor_func_t pDesctructor, int bool){
+
+
+}
+
+
+char *_zend_hash_add_or_update_i(){
+
+
+}
+
+void _zend_hash_apply(){
+
+
+}
+
+
+void zend_hash_apply_with_argument(){
+
+
+}
+
+void zend_hash_apply_with_arguments()
+{
+
+
+}
+
+void _zend_hash_del_el()
+{
+
+
+}
+
+
+
+int main(){
+	printf("assas\n");
+	return 0;
+}
